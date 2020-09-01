e98a5a3c711ea8903e1a2aa82ceaa51c6ef11990
harry <harry@atguigu.com>
2020-09-01 19:42:54 +0800

    modify by master

--- jedistest/src/main/java/GitTest.java
+++ jedistest/src/main/java/GitTest.java
@@ -4,7 +4,7 @@
  */
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello...........");
+        System.out.println("hello..........."); //modify by master
         System.out.println("hello...........");
         System.out.println("hello...........");
         System.out.println("hello...........");
