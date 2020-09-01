8a1c38bc8ccb341016e05820eaacd005949ab074
harry <harry@atguigu.com>
2020-09-01 19:41:58 +0800

    modify by hotfix

--- jedistest/src/main/java/GitTest.java
+++ jedistest/src/main/java/GitTest.java
@@ -4,7 +4,7 @@
  */
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello...........");
+        System.out.println("hello...........");  //modify by hotfix
         System.out.println("hello...........");
         System.out.println("hello...........");
         System.out.println("hello...........");
