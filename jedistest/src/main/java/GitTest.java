c1460a09639e1e56ebd28bdf15418befda8aeb4c
harry <harry@atguigu.com>
2020-09-01 19:26:45 +0800

    do git commit

---
+++ jedistest/src/main/java/GitTest.java
@@ -0,0 +1,15 @@
+/**
+ * @author chenfujiang
+ * @create 2020-09-01 19:25
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello...........");
+        System.out.println("hello...........");
+        System.out.println("hello...........");
+        System.out.println("hello...........");
+        System.out.println("hello...........");
+        System.out.println("hello...........");
+        System.out.println("hello...........");
+    }
+}
